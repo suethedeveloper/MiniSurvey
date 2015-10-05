f05686a6a6335f88909fe5561b66cc36fc576092
Sue Lucas <choi.soyoung@gmail.com>
2015-10-04 17:14:33 -0700

    Refactor save & display quesitons on checkbox

--- main.js
+++ main.js
@@ -9,6 +9,11 @@
   var qHeader = document.getElementById('qHeader');
   var qTypeHeader = document.getElementById('qTypeHeader');
   var multiOptHeader = document.getElementById('multiOptHeader');
+  var qDisplay = document.getElementById('qDisplay');
+  var showSurveyDisplay = document.getElementById('showSurveyDisplay');
+  var innerDiv = "";
+  var idName = "";
+  var showoptArr = [];
 
   function creatSurvey(){
     var surveyName = document.getElementById("surveyName");
@@ -19,18 +24,15 @@
 
     surveyId++;
     surveyName = surveyName.value;
-    var newSurvey = new Survey(surveyId,surveyName);
+    var newSurvey = new Survey(surveyId, surveyName,[]);
     surveyArr.push(newSurvey);
     document.getElementById("createSurveyTitle").style.display = "none";
-    document.getElementById("surveyHeader").innerHTML = surveyName;
+    document.getElementById("surveyHeader").innerHTML = newSurvey.name;
     document.getElementById("createQuestions").style.display = "block"; 
   }
 
   //After a user click 'Create', show options dropdown for question type
   function showOption() {
-    // var qType = document.getElementById('qType').value;
-    // document.getElementById('question').value = "";
-    
     if (qType.value !== "default")
         qTypeHeader.className = "";
 
@@ -46,17 +48,16 @@
   var inputOptionName = "";
 
   function addMoreOption() {
-    if (document.getElementById('option1').value === "" || document.getElementById('option2').value === ""){
-      alert( "Please fill multi choice option first!" );
-      return false;
-    } 
-    inputOptionName = "option" + optionNum;
-    if (optionNum > 2){
-      if (document.getElementById(inputOptionName).value === ""){
-        alert( "Please fill multi choice option first!" );
+    for (var i=1; i <= optionNum; i++) {
+       optionText = document.getElementById('option'+i);
+       multiOptHeader.className = '';
+       if (optionText.value === "") {
+        optionText.focus();
+        multiOptHeader.className += 'error';
         return false;
-      }
-    } 
+       }
+    }    
+
     var newdiv = document.createElement('div');
     optionNum++;
 
@@ -66,7 +67,6 @@
 
   //show previous question & add new question area
   function addMoreQuestion(){
-
     if (qType.value === "default") {  
       qTypeHeader.className += 'error';
 
@@ -87,12 +87,14 @@
     }
 
     var optionsArr = [];
-    currentQuestionArr = surveyArr[surveyArr.length-1]; //get newly created survey object
+    currentArr = surveyArr[surveyArr.length-1]; //get newly created survey object
+    console.log('currentArr',currentArr);
+    currentQuestionArr = currentArr.getQuestionArr();
+
+    console.log('currentQuestionArrxxx',typeof currentQuestionArr);
+    
 
     if (qType.value !== 'openEnd') {
-
-
-
       var optionText = "";
       for (var i=1; i <= optionNum; i++){
          optionText = document.getElementById('option'+i);
@@ -107,26 +109,28 @@
 
       if (qType.value === 'radioButton'){    
         var radioArry = new RadioButton(questionId, question.value, optionsArr);
-        currentQuestionArr.qArr = radioArry;
-        displayQuestionR(currentQuestionArr);
+        currentQuestionArr.push(radioArry);
+        displayQuestionR(currentQuestionArr, qDisplay);
       }
       if (qType.value === 'checkBox'){    
+        
         var checkArry = new CheckBox(questionId, question.value, optionsArr);
-        currentQuestionArr.qArr = checkArry;
+        currentQuestionArr.push(checkArry);
         displayQuestionC(currentQuestionArr);
+
+
       }
       if (qType.value === 'dropDown'){    
         var dropArry = new DropDown(questionId, question.value, optionsArr);
-        currentQuestionArr.qArr = dropArry;
-        displayQuestionD(currentQuestionArr);
+        currentQuestionArr.push(dropArry);
+        displayQuestionD(currentQuestionArr, qDisplay);
       }
     }    
 
     if (qType.value === 'openEnd'){  
-      console.log('questionId',questionId, 'question.value',question.value);
       var openArry = new OpenEnd(questionId, question.value);
-      currentQuestionArr.qArr = openArry;
-      displayQuestionO(currentQuestionArr);
+      currentQuestionArr.push(openArry);
+      displayQuestionO(currentQuestionArr, qDisplay);
     }  
 
     questionId++;
@@ -136,17 +140,24 @@
     document.getElementById('qType').value="default";
     for (var j=1; j <= optionNum; j++){
       document.getElementById('option'+j).value = "";
-    }  
+    } 
+
+    //Show 'Take a Survey button' if any quesiton has created
+    if (surveyArr[0].qArr)
+      document.getElementById('takeSurveyBtn').style.display="inline";
+
+
+console.log('surveyArr',surveyArr);
   }
 
-  function displayQuestionR(optArr){
-    var innerDiv=document.createElement('div');
-    var idName="question" + optArr.qArr.id;
+  function displayQuestionR(optArr, displayDiv){
+    innerDiv = document.createElement('div');
+    idName = "question" + optArr.qArr.id;
     innerDiv.id=idName;
 
-    document.getElementById('qDisplay').appendChild(innerDiv); 
-    var showoptArr = optArr.qArr.optionsArr;
-      var inputRadio="<p>" + optArr.qArr.id+". " + optArr.qArr.question + "<br>";
+    displayDiv.appendChild(innerDiv); 
+    showoptArr = optArr.qArr.optionsArr;
+    var inputRadio="<p>" + optArr.qArr.id+". " + optArr.qArr.question + "<br>";
     for(i=0;i<showoptArr.length;i++){
     inputRadio += "<input id='showoptArr[i]' name='radioB' type='radio'> " + showoptArr[i]+"<br>";
     }
@@ -154,28 +165,30 @@
   }
 
   function displayQuestionC(optArr){
+    //get last item in question array
+    var curIndex = optArr.length-1;
     var innerDiv=document.createElement('div');
-    var idName="question" + optArr.qArr.id;
+    idName = "question" + optArr[curIndex].id;
     innerDiv.id=idName;
 
     document.getElementById('qDisplay').appendChild(innerDiv); 
-    var showoptArr = optArr.qArr.optionsArr;
-      var inputCheck="<p>" + optArr.qArr.id+". " + optArr.qArr.question + "<br>";
-    for(i=0;i<showoptArr.length;i++){
-    inputCheck += "<input id='showoptArr[i]' name='checkB' type='checkbox'> " + showoptArr[i]+"<br>";
+    showoptArr = optArr[curIndex].optionsArr;
+    var inputCheck = "<p>" + optArr[curIndex].id+". " + optArr[curIndex].question + "<br>";
+
+    for(i=0;i<showoptArr.length;i++) {
+      inputCheck += "<input id='"+showoptArr[i]+"' name='checkB' type='checkbox'> " + showoptArr[i] + "<br>";
     }
     document.getElementById(idName).innerHTML = inputCheck;
   }
 
-  function displayQuestionD(optArr){
-    var innerDiv=document.createElement('div');
-    var idName="question" + optArr.qArr.id;
+  function displayQuestionD(optArr, displayDiv){
+    innerDiv = document.createElement('div');
+    idName = "question" + optArr.qArr.id;
     innerDiv.id=idName;
+    qDisplay.appendChild(innerDiv); 
 
-    document.getElementById('qDisplay').appendChild(innerDiv); 
-
-    var showoptArr = optArr.qArr.optionsArr;
-    var inputDrop="<p style='margin:10px'>" + optArr.qArr.id +". "+ optArr.qArr.question + "<br>";
+    showoptArr = optArr.qArr.optionsArr;
+    var inputDrop = "<p>" + optArr.qArr.id +". "+ optArr.qArr.question + "<br>";
 
     inputDrop+="<select style='margin:10px' id='showoptArr[i]' style='margin:10px'>";
     inputDrop += "<option value='default'>----</option>";
@@ -186,38 +199,36 @@
     document.getElementById(idName).innerHTML=inputDrop;
   }
 
-  function displayQuestionO(optArr){
-    console.log('open');
-    var innerDiv=document.createElement('div');
-    console.log('innerDiv',innerDiv);
-    var idName="question" + optArr.qArr.id;
-    console.log('idName',idName);
+  function displayQuestionO(optArr, displayDiv){
+    innerDiv = document.createElement('div');
+    idName = "question" + optArr.qArr.id;
     innerDiv.id=idName;
-
-    document.getElementById('qDisplay').appendChild(innerDiv); 
-    var showoptArr = optArr.qArr.optionsArr;
-    var inputText="<p>"+ optArr.qArr.id +". " + optArr.qArr.question + "<br>";
+    displayDiv.appendChild(innerDiv); 
+    showoptArr = optArr.qArr.optionsArr;
+    var inputText = "<p>"+ optArr.qArr.id +". " + optArr.qArr.question + "<br>";
     inputText+="<input type='text'>";
     document.getElementById(idName).innerHTML = inputText;
   }
 
   function takeSurvey(){ 
-    if (qType.value === "default") {
-      alert("Please Select Question Type.");
-      qType.focus();
-      return false;
-    }  
-    if (document.getElementById('question').value === "") {
-      alert("What question do you want to ask?");
-      document.getElementById('question').focus();
-      return false;
-    }
+    if (qType.value !== "default" || question.value !== "")
+      confirm("Are you sure not to finish creating the survey first?");
+
+    document.getElementById("createQuestions").style.display = "none"; 
+    document.getElementById("showSurvey").style.display = "block"; 
+
+    console.log("surveyArr",surveyArr);
+    console.log("surveyArr",surveyArr[0]);
+
+    // innerDiv = document.createElement('div');
+
+    // showSurveyDisplay.
+
   }
 
   var checkSurvey = function(){
     if (qType.value === "default") {  
       qTypeHeader.className += 'error';
-
       qType.focus();
       return false;
     } else {
@@ -227,7 +238,6 @@
     if (question.value === "") {    
       if(qHeader) 
         qHeader.className += 'error';
-
       question.focus();
       return false;
     } else {
@@ -235,26 +245,3 @@
     }
     return true;
   };
-
-  // var checkSurvey = function() {
-  //   if (qType.value === "default") {  
-  //     qTypeHeader.className += ' error';
-
-  //     qType.focus();
-  //     return false;
-  //   } else {
-  //     qTypeHeader.className -= ' error';
-  //   }
-
-  //   if (question.value === "") {    
-  //     if(qHeader) 
-  //       qHeader.className += ' error';
-
-  //     question.focus();
-  //     return false;
-  //   } else {
-  //     qHeader.className -= ' error';
-  //   }
-  // };
-
-// });
